dd72b26891b5e4b5e788a2a489fd7e27181e1882
abhishekKappala <abhishekbunny.001@gmail.com>
2026-05-31 23:05:21 +0530

    add hf token debug logs

--- backend/app/services/embedding_service.py
+++ backend/app/services/embedding_service.py
@@ -14,6 +14,14 @@ def get_client():
 
     global _client
 
+    print("HF TOKEN EXISTS:", bool(settings.HF_API_TOKEN))
+
+    if settings.HF_API_TOKEN:
+        print(
+            "HF TOKEN PREFIX:",
+            settings.HF_API_TOKEN[:10]
+        )
+
     if _client is None:
 
         _client=InferenceClient(
